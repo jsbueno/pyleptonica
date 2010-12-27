ba98616a2c6505a77bae31a2b4257ed8fc7c5e86
Joao S. O. Bueno <gwidion@mpc.com.br>
2010-12-27 21:25:47 -0200

    Updates sample program to wrok with generated structs

--- leptonica.py
+++ leptonica.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import ctypes
+import struct
+from lepton_ctypes import PIX
 
 # from ctypes import c_* #invlaid python, so we do this:
 globals().update((name, getattr(ctypes, name)) for name in dir(ctypes) if name.startswith("c_"))
@@ -7,36 +9,6 @@ globals().update((name, getattr(ctypes, name)) for name in dir(ctypes) if name.s
 lep = ctypes.cdll.LoadLibrary( "liblept.so")
 
 
-class PIX(ctypes.Structure):
-    """
-    struct Pix
-    {
-        l_uint32             w;           /* width in pixels                   */
-        l_uint32             h;           /* height in pixels                  */
-        l_uint32             d;           /* depth in bits                     */
-        l_uint32             wpl;         /* 32-bit words/line                 */
-        l_uint32             refcount;    /* reference count (1 if no clones)  */
-        l_uint32             xres;        /* image res (ppi) in x direction    */
-                                          /* (use 0 if unknown)                */
-        l_uint32             yres;        /* image res (ppi) in y direction    */
-                                          /* (use 0 if unknown)                */
-        l_int32              informat;    /* input file format, IFF_*          */
-        char                *text;        /* text string associated with pix   */
-        struct PixColormap  *colormap;    /* colormap (may be null)            */
-        l_uint32            *data;        /* the image data                    */
-    };"""
-    _fields_ = [
-        ("w", c_uint32),
-        ("h", c_uint32),
-        ("d", c_uint32),
-        ("wpl", c_uint32),
-        ("refcount", c_uint32),
-        ("xres", c_uint32),
-        ("yres", c_uint32),
-        ("informat", c_uint32),
-        ("text", c_char_p),
-        ("colormap", c_void_p), #TODO: PixColormap struct
-        ("data", ctypes.POINTER(c_ubyte))]
 
 def getPix(pointer):
     if pointer is None:
@@ -56,8 +28,11 @@ if __name__ == "__main__":
     sc = pygame.display.set_mode((img.w, img.h))
     for y in xrange(img.h):
       for x in xrange(img.w):
-         a,b,g,r = (img.data[y * img.wpl * 4+ 4 * x + i] for i in (0,1,2,3))
-         sc.set_at((x,y), (r,g,b,a))
+         color_int = img.data[y * img.wpl + x]
+         # Fake big endian, to retrieve the data in the order needed for pygame
+         color_str = struct.pack(">I", color_int)
+         color_tup = struct.unpack("BBBB", color_str)
+         sc.set_at((x,y), color_tup)
    
     pygame.display.flip()
     raw_input()
